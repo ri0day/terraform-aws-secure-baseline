d6a8dfc5481c071b377dd6370982a8a9f3c44329
mason.wu <mason.wu@shijigroup.com>
2022-10-26 17:05:03 +0800

    cn demo

--- examples/aws-china-region/main.tf
+++ examples/aws-china-region/main.tf
@@ -12,12 +12,12 @@ provider "aws" {
   }
 
 module "secure_baseline" {
-  source = "/Users/min/repos/foxhis-gms/local-modules/secure-baseline-cn"
-  audit_log_bucket_name           = var.audit_s3_bucket_name
-  aws_account_id                  = var.account_id
-  region                          = var.default_region
-  support_iam_role_principal_arns = var.support_users
-  target_regions                  = var.target_regions
+  source = "../../"
+  audit_log_bucket_name           = "demobucket-ioyz9"
+  aws_account_id                  = "123456789012"
+  region                          = "cn-northwest-1"
+  support_iam_role_principal_arns = "arn:aws-cn:iam::123456789012:user/user1"
+  target_regions                  = ["cn-northwest-1","cn-north-1"]
 
   audit_log_bucket_force_destroy = true
   providers = {
